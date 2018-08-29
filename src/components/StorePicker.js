f369b9a3c9956751451b5ccf67f8e3c1b9029746
Jorge Jimenez <xadrijo@gmail.com>
2018-08-29 16:36:03 -0300

    Check events, refs and bind

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+  myInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    console.log(this);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store -></button>
       </form>
     );
